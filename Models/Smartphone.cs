a85ec1e856b30f68231f4afbc1db29d7d831bd64
Gledson Paiva <gleds.hpaiva@gmail.com>
2024-06-01 13:43:16 -0300

    Prontinho

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,18 @@
+using System;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string marca)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Marca = marca;
         }
 
         public void Ligar()
@@ -22,5 +26,25 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+    }
+
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo) : base(numero, modelo, "Nokia") { }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} na loja da Nokia...");
+        }
+    }
+
+    public class IPhone : Smartphone
+    {
+        public IPhone(string numero, string modelo) : base(numero, modelo, "iPhone") { }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} na App Store...");
+        }
     }
 }
